a28a0117a88f5a3331785610716048c617cd3ac3
Rachel Roar <rachel_roar@outlook.com>
2020-01-23 10:44:30 -0500

    I'm starting a new day, I made more progress on Grammar.ts

---
+++ 01-regex/Grammar.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var Grammar = /** @class */ (function () {
+    function Grammar(gramgram) {
+        var g = gramgram.split('\n');
+        var bBoi = new Set();
+        g.forEach(function (element) {
+            bBoi.add(element);
+        });
+        //print(g)
+    }
+    return Grammar;
+}());
+exports.Grammar = Grammar;
--- 01-regex/Grammar.ts
+++ 01-regex/Grammar.ts
@@ -1,6 +1,11 @@
 export class Grammar 
 {
     constructor(gramgram : string){
-        
+        let g = gramgram.split('\n')
+        let bBoi : Set<string> = new Set();
+        g.forEach(element => {
+            bBoi.add(element);
+        });
+        //print(g)
     }
 }
